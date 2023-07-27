7c0692f56d0163c2f04c875718f746b8ec03117f
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-07-28 00:06:07 +0900

    Feature getListOfFavoriteLectureRoomsAndLectures

--- backend/src/main/java/com/hot6/pnureminder/controller/LectureRoomController.java
+++ backend/src/main/java/com/hot6/pnureminder/controller/LectureRoomController.java
@@ -2,11 +2,16 @@ package com.hot6.pnureminder.controller;
 
 import com.hot6.pnureminder.dto.LectureRoom.*;
 import com.hot6.pnureminder.entity.Building;
+import com.hot6.pnureminder.entity.Member;
 import com.hot6.pnureminder.repository.BuildingRepository;
 import com.hot6.pnureminder.service.BuildingService;
+import com.hot6.pnureminder.service.Favorite.FavoriteBuildingService;
 import com.hot6.pnureminder.service.LectureRoomService;
+import com.hot6.pnureminder.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalTime;
@@ -19,8 +24,10 @@ import java.util.List;
 public class LectureRoomController {
 
     private final LectureRoomService lectureRoomService;
+    private final MemberService memberService;
     private final BuildingService buildingService;
     private final BuildingRepository buildingRepository;
+    private final FavoriteBuildingService favoriteBuildingService;
 
     @GetMapping("/available")
     public ResponseEntity<BuildingResponseDto> getAvailableLectureRoomsAndLectures(
@@ -87,7 +94,42 @@ public class LectureRoomController {
 
         return ResponseEntity.ok(responses);
     }
+    @GetMapping("/favorite-list")
+    public ResponseEntity<List<BuildingResponseDto>> getListOfFavoriteLectureRoomsAndLectures(
+            @RequestParam Integer setTime
+    ) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
 
+        Member member = memberService.findMemberByUsername(username);
+        List<Building> buildings = favoriteBuildingService.findBuildingByMember(member);
+        List<BuildingResponseDto> responses = new ArrayList<>();
+
+        for (Building building : buildings) {
+            String buildingName = building.getBuildingName();
+
+            LectureRoomRequestDto requestDto = LectureRoomRequestDto.builder()
+                    .buildingName(buildingName)
+                    .currentTime(LocalTime.of(14, 40))  //임시 시간
+                    .setTime(setTime)
+                    .dayOfWeek(0)
+                    .build();
+
+            List<AvailableNowDto> nowAvailableResponse= lectureRoomService.getNowAvailableLectureRoomsAndLectures(requestDto);
+
+            if (!nowAvailableResponse.isEmpty()) {
+                BuildingResponseDto buildingResponseDto = BuildingResponseDto.builder()
+                        .buildingName(buildingName)
+                        .availableNow(nowAvailableResponse)
+                        .buildingLat(building.getBuildingLat())
+                        .buildingLng(building.getBuildingLng())
+                        .build();
+                responses.add(buildingResponseDto);
+            }
+        }
+
+        return ResponseEntity.ok(responses);
+    }
 
 }
 
